8352be89a060f394c47be9af91d9a2194ea673cf
Jose Gregorio Materan <josemateran1998@gmail.com>
2022-05-06 19:45:34 -0400

    feat: added progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,36 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+
+import '../styles/globals.css'
+
+import Router from 'next/router'
+
+import 'nprogress/nprogress.css'
+import NProgress from 'nprogress'
 
 function MyApp ({ Component, pageProps }: AppProps) {
+  useEffect(
+    () => {
+      const handleRouteStart = () => {
+        NProgress.start()
+      }
+
+      const handleRouteDone = () => {
+        NProgress.done()
+      }
+
+      Router.events.on('routeChangeStart', handleRouteStart)
+      Router.events.on('routeChangeComplete', handleRouteDone)
+      Router.events.on('routeChangeError', handleRouteDone)
+
+      return () => {
+        Router.events.off('routeChangeStart', handleRouteStart)
+        Router.events.off('routeChangeComplete', handleRouteDone)
+        Router.events.off('routeChangeError', handleRouteDone)
+      }
+    },
+    []
+  )
   return (
     <Component {...pageProps} />
   )
