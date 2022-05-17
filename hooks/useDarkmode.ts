59d01c0254f85d33d8bf536190c63996c16ad936
Jose Gregorio Materan <josemateran1998@gmail.com>
2022-05-17 10:53:36 -0400

    refactor: unnecessary files have been removed

--- hooks/useDarkmode.ts
+++
@@ -1,6 +0,0 @@
-import { useEffect } from 'react'
-import { useLocalStorage } from '@/hooks/useLocalStorage'
-
-export function useDarkmode () {
-  return null
-}
--- pages/other.tsx
+++
@@ -1,9 +0,0 @@
-import { HomeLayout } from '@/layouts/HomeLayout'
-
-export default function Other () {
-  return (
-    <HomeLayout>
-      <h1>Other</h1>
-    </HomeLayout>
-  )
-}
