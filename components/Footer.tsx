a69a83e50b00270ba60b7a68b36cb39b6a6426e4
Jose Gregorio Materan <josemateran1998@gmail.com>
2022-06-12 16:24:05 -0400

    refactor: social links added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 import {
-  Heart,
-  Coffee
+  Linkedin,
+  GitHub,
+  Twitter
 } from 'react-feather'
 
 import { DarkmodeButton } from '@/components/DarkmodeButton'
@@ -9,7 +10,20 @@ export function Footer () {
   return (
     <div className="flex items-center mt-14 pb-14 text-white">
       <div className="flex items-center">
-        Make with <Heart className="mx-2" /> and a lot of <Coffee className="mx-2" />
+        <span className="mr-4 text-sm uppercase text-gray-200 font-bold">
+          Follow me on
+        </span>
+        <div className="flex items-center space-x-3">
+          <a href="#">
+            <Twitter className="w-5 h-5" />
+          </a>
+          <a href="#">
+            <GitHub className="w-5 h-5" />
+          </a>
+          <a href="#">
+            <Linkedin className="w-5 h-5" />
+          </a>
+        </div>
       </div>
       <div className="flex-1"></div>
       <div>
