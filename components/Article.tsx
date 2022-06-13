6a7d6ff112428d1c6bf700d64d80ef3dd13c8ad5
Jose Gregorio Materan <josemateran1998@gmail.com>
2022-06-12 21:23:40 -0400

    feat: minimal changes

--- components/Article.tsx
+++ components/Article.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 
+import { ExternalLink } from 'react-feather'
+
 import { format, parseISO } from 'date-fns'
 
 import { Heading } from '@/components/Heading'
@@ -31,17 +33,20 @@ export function Article ({
   return (
     <article className="px-6 mb-8">
       <Link href={`/blog/${slug}`}>
-        <a className="block focus:outline-none focus-visible:ring-2 p-10 -mx-6 border bg-slate-200 dark:bg-gray-800 rounded-lg border-slate-400 border-dotted dark:border-gray-600">
+        <a className="hover:scale-105 duration-150 block focus:outline-none focus-visible:ring-2 p-10 -mx-6 border bg-slate-200 dark:bg-gray-800 rounded-lg border-slate-400 border-dotted dark:border-gray-600">
           <div className="flex md:justify-start items-center justify-between mb-6 text-sm text-slate-400">
             <div className="md:w-36">
               <Text>
                 {formatDate(date)}
               </Text>
             </div>
-            <div>
-              <Text>
+            <div className="flex items-center space-x-4 md:flex-1">
+              <Text className="flex-1">
                 {read}
               </Text>
+              <button tabIndex={-1} className="text-black dark:text-white">
+                <ExternalLink className="w-5 h-5" />
+              </button>
             </div>
           </div>
           <div className="md:pl-36">
@@ -52,7 +57,7 @@ export function Article ({
               {description}
             </Text>
             <div className="mt-8">
-              <Button tabIndex="-1">
+              <Button tabIndex={-1}>
                 Read more
               </Button>
             </div>
