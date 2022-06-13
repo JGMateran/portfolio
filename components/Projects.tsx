16f088a2c3f2582ed6aeb6383f60d7dbc3cec97c
Jose Gregorio Materan <josemateran1998@gmail.com>
2022-06-13 17:58:13 -0400

    refactor: project rewritted to use map

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,6 +5,16 @@ import { Text } from '@/components/Text'
 
 import { GitHub, ExternalLink } from 'react-feather'
 
+type ProjectTypes = {
+  id: number,
+  title: string,
+  description: string,
+  image: string,
+  url: string,
+  github: string,
+  technologies: string[]
+}
+
 function Badge ({ children }: { children: ReactNode }) {
   return (
     <span className="inline-flex text-xs py-2 px-3 rounded-full border border-dotted border-gray-600">
@@ -13,21 +23,18 @@ function Badge ({ children }: { children: ReactNode }) {
   )
 }
 
-type ProjectProps = {
-  title: string,
-  description: string,
-  image: string
-}
-
 function Project ({
   title,
   description,
-  image
-}: ProjectProps) {
+  image,
+  url,
+  github,
+  technologies
+}: Omit<ProjectTypes, 'id'>) {
   return (
     <div className="md:flex md:items-center bg-slate-200 dark:bg-gray-800 border border-dotted border-slate-400 dark:border-gray-600 rounded-lg mb-8">
       <div className="md:w-1/2">
-        <a href="#">
+        <a href={url} rel="noopener noreferrer" target="_blank">
           <img src={image} className="rounded-lg" alt="" />
         </a>
       </div>
@@ -36,10 +43,10 @@ function Project ({
           <Heading size="small" className="flex-1 truncate">
             {title}
           </Heading>
-          <a href="#" className="ml-4">
+          <a href={github} className="ml-4" rel="noopener noreferrer" target="_blank">
             <GitHub className="w-5 h-5" />
           </a>
-          <a href="#" className="ml-4">
+          <a href={url} className="ml-4" rel="noopener noreferrer" target="_blank">
             <ExternalLink className="w-5 h-5" />
           </a>
         </div>
@@ -47,17 +54,61 @@ function Project ({
           {description}
         </Text>
         <div className="flex items-center space-x-2">
-          <Badge>
-            Vite
-          </Badge>
-          <Badge>
-            TailwindCSS
-          </Badge>
+          {
+            technologies.map((technology, index) => (
+              <Badge key={index}>{technology}</Badge>
+            ))
+          }
         </div>
       </div>
     </div>
   )
 }
+
+const projects: ProjectTypes[] = [
+  {
+    id: 0,
+    title: 'Movie Search',
+    description: 'A search engine for movies.',
+    image: '/images/01.png',
+    url: 'https://vite-tmdb-explorer.vercel.app/',
+    github: 'https://github.com/JGMateran/tmdb-exporer',
+    technologies: [
+      'React',
+      'TypeScript',
+      'Vite',
+      'TailwindCSS'
+    ]
+  },
+  {
+    id: 1,
+    title: 'Beautifull Twitter Cards',
+    description: 'A simple tool to generate Twitter Cards.',
+    image: '/images/02.png',
+    url: 'https://beautiful-twitter-cards.vercel.app/',
+    github: 'https://github.com/JGMateran/beautiful-twitter-cards',
+    technologies: [
+      'React',
+      'TypeScript',
+      'Vite',
+      'TailwindCSS'
+    ]
+  },
+  {
+    id: 2,
+    title: 'Open-Source URL Shortener',
+    description: 'A simple tool to shorten URLs.',
+    image: '/images/03.png',
+    url: '#',
+    github: '#',
+    technologies: [
+      'React',
+      'Next.js',
+      'TailwindCSS',
+      'Supabase'
+    ]
+  }
+]
 
 export function Projects () {
   return (
@@ -66,23 +117,17 @@ export function Projects () {
         Personal projects
       </Heading>
 
-      <Project
-        title="Dolor consequuntur aspernatur optio a"
-        description="Dolor adipisicing explicabo provident totam reiciendis Dolorum ab iusto minus!"
-        image="/images/01.png"
-      />
+      {
+        projects.map(({ id, ...rest }) => (
+          <Project key={id} {...rest} />
+        ))
+      }
 
-      <Project
-        title="Dolor consequuntur aspernatur optio a"
-        description="Dolor adipisicing explicabo provident totam reiciendis Dolorum ab iusto minus!"
-        image="/images/02.png"
-      />
-
-      <Project
-        title="Dolor consequuntur aspernatur optio a"
-        description="Dolor adipisicing explicabo provident totam reiciendis Dolorum ab iusto minus!"
-        image="/images/03.png"
-      />
+      <div className="text-center mt-10 mb-14">
+        <a href="#" className="font-semibold">
+          Ver todos los proyectos
+        </a>
+      </div>
     </>
   )
 }
