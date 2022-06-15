5c68c3574185f71fcd79f3cb98e5ff02171684be
Jose Gregorio Materan <josemateran1998@gmail.com>
2022-06-15 16:13:11 -0400

    feat: favicon added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="icon" href="/images/favicon.svg" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Inter:wght@400;500;700;800&display=swap" rel="stylesheet" />
