98368cf6c3f25a54c4e0f3ce91e96da6b048449b
Jose Gregorio Materan <josemateran1998@gmail.com>
2022-07-13 15:07:35 -0400

    feat: analytics added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,8 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Inter:wght@400;500;700;800&display=swap" rel="stylesheet" />
+
+          <script async defer data-website-id="853d84ff-d935-4bec-85e7-2a02112e7db8" src="https://umami-rosy-theta.vercel.app/umami.js"></script>
         </Head>
 
         <body className="dark:bg-gray-900">
