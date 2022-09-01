fc6e3b1294a3a67cdade1de770f832d2dd15b916
Jose Gregorio Materan <josemateran1998@gmail.com>
2022-08-31 22:38:52 -0400

    feat: drawer added

--- components/BurgerButton/BurgerButton.tsx
+++ components/BurgerButton/BurgerButton.tsx
@@ -5,16 +5,20 @@ import style from './BurgerButton.module.css'
 import clsx from 'clsx'
 
 type BurgerButtonProps = ComponentPropsWithoutRef<'button'> & {
-  open: boolean
+  open: boolean,
 }
 
 export function BurgerButton ({
   open,
+  className,
   ...props
 }: BurgerButtonProps) {
   return (
     <button
-      className="w-10 h-10 rounded-full focus:outline-none focus-visible:ring-2 flex items-center justify-center hover:cursor-pointer"
+      className={clsx(
+        'inline-block w-10 h-10 rounded-full focus:outline-none focus-visible:ring-2 flex items-center justify-center hover:cursor-pointer',
+        className
+      )}
       {...props}
     >
       <span
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,81 @@
+import { useState } from 'react'
+
 import Link from 'next/link'
 
 import { Container } from '@/components/Container'
 import { Anchor } from '@/components/Anchor'
 import { Button } from '@/components/Button'
+import { BurgerButton } from '@/components/BurgerButton'
+
+import clsx from 'clsx'
+
+const items = [
+  {
+    key: 0,
+    content: 'Blog',
+    href: '/blog'
+  },
+  {
+    key: 1,
+    content: 'Curriculum',
+    href: '/curriculum-english.pdf'
+  }
+]
+
+function Drawer ({ open }: { open: boolean }) {
+  return (
+    <div className={clsx(
+      {
+        'invisible sm:visible': !open,
+        visible: open
+      }
+    )}>
+      <div
+        aria-hidden="true"
+        className={clsx(
+          'fixed inset-0 duration-200 sm:hidden',
+          {
+            'visible bg-gray-900/90': open,
+            'invisible bg-translate': !open
+          }
+        )}
+      />
+      <div className="flex text-2xl fixed inset-0 flex-col p-10 justify-center items-center space-y-6 font-bold sm:relative sm:text-sm sm:font-normal sm:justify-start sm:items-center sm:flex-row sm:space-y-0 sm:space-x-4 sm:p-0">
+        {
+          items.map((item, index) => (
+            <Anchor
+              key={item.key}
+              href={item.href}
+              className={clsx(
+                'duration-200',
+                {
+                  'translate-x-0 sm:opacity-100 sm:translate-0': open,
+                  'opacity-0 -translate-x-4 sm:opacity-100 sm:translate-0': !open
+                }
+              )}
+              style={{
+                transitionDelay: `${index * 100}ms`
+              }}
+            >
+              {item.content}
+            </Anchor>
+          ))
+        }
+        <Button
+          as="a"
+          href="#contact"
+          className="hidden sm:block"
+        >
+          Contact me
+        </Button>
+      </div>
+    </div>
+  )
+}
 
 export function Navbar () {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <div className="sticky top-0 z-30">
       <Container className="flex items-center h-14 px-6">
@@ -13,11 +84,13 @@ export function Navbar () {
             reburn.dev
           </Link>
         </h2>
-        <div className="flex items-center text-sm space-x-4 dark:text-gray-300 text-black">
-          <Anchor href="/blog">Blog</Anchor>
-          <Anchor href="/curriculum-english.pdf">Curriculum</Anchor>
-          <Button as="a" href="#contact">Contact me</Button>
-        </div>
+        {/* <div className="flex items-center text-sm space-x-4 dark:text-gray-300 text-black" /> */}
+        <Drawer open={isOpen} />
+        <BurgerButton
+          className="relative z-40 sm:hidden"
+          open={isOpen}
+          onClick={() => setIsOpen((prev) => !prev)}
+        />
       </Container>
     </div>
   )
