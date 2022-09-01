ac010895ad141a4cbbb8944006243fce3f7693a6
Jose Gregorio Materan <josemateran1998@gmail.com>
2022-08-31 23:00:38 -0400

    refactor: correct menu size and color

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -40,14 +40,14 @@ function Drawer ({ open }: { open: boolean }) {
           }
         )}
       />
-      <div className="flex text-2xl fixed inset-0 flex-col p-10 justify-center items-center space-y-6 font-bold sm:relative sm:text-sm sm:font-normal sm:justify-start sm:items-center sm:flex-row sm:space-y-0 sm:space-x-4 sm:p-0">
+      <div className="flex text-xl fixed inset-0 flex-col p-10 justify-center items-center space-y-6 font-bold sm:relative sm:text-sm sm:font-normal sm:justify-start sm:items-center sm:flex-row sm:space-y-0 sm:space-x-4 sm:p-0">
         {
           items.map((item, index) => (
             <Anchor
               key={item.key}
               href={item.href}
               className={clsx(
-                'duration-200',
+                'duration-200 text-white',
                 {
                   'translate-x-0 sm:opacity-100 sm:translate-0': open,
                   'opacity-0 -translate-x-4 sm:opacity-100 sm:translate-0': !open
