0309663d192c4b75d74db5af1c68f9fffb3a9c5d
José Gregorio Materán <josemateran1998@gmail.com>
2022-12-14 11:02:06 -0400

    fix: now the card doesn't callide with the blur

--- components/ArticlesList.tsx
+++ components/ArticlesList.tsx
@@ -21,7 +21,7 @@ export function ArticlesList ({
 
   return (
     <Container size="small" gap={false}>
-      <div className="grid grid-cols-2 sm:grid-cols-2 grid-cols-1 gap-10">
+      <div className="grid pt-3 sm:grid-cols-2 grid-cols-1 gap-10">
         {
           data.map((article) => (
             <ArticleItem key={article.slug} {...article} />
