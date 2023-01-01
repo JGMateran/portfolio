49f09baef7ee2bbf7a9a30f38a491d5313dd81bb
José Gregorio Materán <josemateran1998@gmail.com>
2023-01-01 15:53:49 -0400

    fix: lang was changed to english

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ import Document, {
 export default class MyDocument extends Document {
   render () {
     return (
-      <Html lang="es">
+      <Html lang="en">
         <Head>
           <script async defer data-website-id="fc50a674-0289-4fec-8873-ce79dfca03fa" src="https://umami-qa34psfio-reburn.vercel.app/umami.js"></script>
         </Head>
