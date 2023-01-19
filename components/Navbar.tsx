84ea23c971c932158ebf7709c9fbb65c9fb53830
José Gregorio Materán <josemateran1998@gmail.com>
2023-01-18 21:46:32 -0400

    feat: the drawer component moved to its own file

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,11 +3,8 @@ import { useEffect, useState, useRef } from 'react'
 import Link from 'next/link'
 
 import { Container } from '@/components/Container'
-import { Anchor } from '@/components/Anchor'
-import { Button } from '@/components/Button'
 import { BurgerButton } from '@/components/BurgerButton'
-
-import clsx from 'clsx'
+import { Drawer } from './Drawer'
 
 const items = [
   {
@@ -21,54 +18,6 @@ const items = [
     href: '/curriculum-english.pdf'
   }
 ]
-
-function Drawer ({ open }: { open: boolean }) {
-  return (
-    <div className={clsx(
-      {
-        'invisible sm:visible': !open,
-        visible: open
-      }
-    )}>
-      <div
-        aria-hidden="true"
-        className={clsx(
-          'fixed inset-0 duration-200 sm:hidden',
-          {
-            'visible bg-gray-900/90': open,
-            'invisible bg-transparent': !open
-          }
-        )}
-      />
-      <div className="flex text-xl fixed inset-0 flex-col p-10 justify-center items-center space-y-6 font-bold sm:relative sm:text-sm sm:font-normal sm:justify-start sm:items-center sm:flex-row sm:space-y-0 sm:space-x-4 sm:p-0">
-        {
-          items.map((item) => (
-            <Anchor
-              key={item.key}
-              href={item.href}
-              className={clsx(
-                'duration-200 text-white sm:dark:text-white sm:text-black',
-                {
-                  'translate-x-0 sm:opacity-100 sm:translate-0': open,
-                  'opacity-0 -translate-y-2 sm:opacity-100 sm:translate-y-0': !open
-                }
-              )}
-            >
-              {item.content}
-            </Anchor>
-          ))
-        }
-        <Button
-          as="a"
-          href="#contact"
-          className="hidden sm:block"
-        >
-          Contact me
-        </Button>
-      </div>
-    </div>
-  )
-}
 
 export function Navbar () {
   const [isOpen, setIsOpen] = useState(false)
@@ -109,7 +58,7 @@ export function Navbar () {
 
         <div className="flex-1"></div>
 
-        <Drawer open={isOpen} />
+        <Drawer open={isOpen} items={items} />
 
         <BurgerButton
           className="relative z-40 sm:hidden"
