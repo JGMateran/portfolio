25278db130edc47a2c1a5b3f89c77ed102b93f68
José Gregorio Materán <josemateran1998@gmail.com>
2023-03-27 00:35:43 -0400

    feat: added priority for avatar image

--- layouts/HomeLayout.tsx
+++ layouts/HomeLayout.tsx
@@ -27,6 +27,7 @@ export function HomeLayout ({
                 width="240"
                 height="240"
                 alt="José Gregorio Materán"
+                priority
               />
             </div>
           </div>
