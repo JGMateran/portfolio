bd0d9f2e1d51df500af63e22d5ca3ca013779bd2
José Gregorio Materán <josemateran1998@gmail.com>
2023-11-09 12:33:26 -0400

    feat: migrate to native sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,23 @@
+import { allPosts } from '.contentlayer/generated'
+import { type MetadataRoute } from 'next'
+
+export default function sitemap (): MetadataRoute.Sitemap {
+  const routes = ['/', '/blog'].map((route) => {
+    return {
+      url: `https://reburn.dev${route}`,
+      lastModified: new Date().toISOString().split('T')[0]
+    }
+  })
+
+  const posts = allPosts.map((post) => {
+    return {
+      url: `https://reburn.dev/blog/${post.slug}`,
+      lastModified: post.publishedAt
+    }
+  })
+
+  return [
+    ...routes,
+    ...posts
+  ]
+}
