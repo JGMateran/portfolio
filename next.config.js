e522c581f190a15dab9ca36ab91b64ddfecc3024
José Gregorio Materán <josemateran1998@gmail.com>
2023-11-11 09:26:55 -0400

    feat: update to new version of contentlayer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-/** @type {import('next').NextConfig} */
-
 const { withContentlayer } = require('next-contentlayer')
 
-module.exports = withContentlayer({
-  reactStrictMode: false
-})
+/** @type {import('next').NextConfig} */
+const nextConfig = { 
+  reactStrictMode: true, 
+  swcMinify: true 
+}
+
+module.exports = withContentlayer(nextConfig)
