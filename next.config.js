42ef242fbb5c0b4736123838e2a584affb562bb8
José Gregorio Materán <josemateran1998@gmail.com>
2024-08-31 23:39:36 -0400

    feat: migrate to content collections

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const { withContentlayer } = require('next-contentlayer')
+const { withContentCollections } = require('@content-collections/next')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = { 
@@ -6,4 +6,4 @@ const nextConfig = {
   swcMinify: true 
 }
 
-module.exports = withContentlayer(nextConfig)
+module.exports = withContentCollections(nextConfig)
